10199d160b42b354618d5142d417217ffb27b106
KavindiSirimanna <sirimannakavindi@gmail.com>
2025-09-17 15:16:56 +0530

    stockRoutes

--- Backend/Routes/StockRoutes.js
+++ Backend/Routes/StockRoutes.js
@@ -1,1 +1,14 @@
-r
+const express = require("express");
+const router = express.Router();
+const Stock = require("../Controllers/Stock");
+
+router.get("/", Stock.getAllStocks);
+router.get("/fuelLevels", Stock.getCurrentStockLevels );
+router.post("/", Stock.addStocks);
+router.get("/:id", Stock.getById);
+router.put("/:id", Stock.updateStock);
+router.delete("/:id", Stock.deleteStock);
+
+
+
+module.exports = router;
