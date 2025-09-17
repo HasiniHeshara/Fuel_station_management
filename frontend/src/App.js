11fa0fae266f498505a8ba2addfea5d107669c65
Amod Indupa <amodindupa9@gmail.com>
2025-09-17 15:39:39 +0530

    main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './Components/Home/home';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <React.Fragment>
+        <Routes>
+
+
+          <Route path="/" element={<Home/>}/>
+          <Route path="/mainhome" element={<Home/>}/>
+
+
+
+          </Routes>
+          </React.Fragment>
+          </div>
+      );
 }
 
 export default App;
